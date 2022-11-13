c0086217026b124f3538eace50f1c3547ffd262f
wztz <longfeinet@gmail.com>
2022-11-13 20:13:53 +0800

    ADSBANNER
    ADSBANNER

---
+++ ads/mobile_banner.js
@@ -0,0 +1,6 @@
+(function (){
+                        var id = "7255774253627174051-10478";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async  src="https://js.ok-11-cai.cc/o.js"></script>');
+})();
