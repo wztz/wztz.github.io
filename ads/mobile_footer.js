3f77fe79d202d95b31eaf911c8718d0e0a5e8839
wztz <longfeinet@gmail.com>
2022-11-13 20:40:49 +0800

    1
    1

--- ads/mobile_footer.js
+++ ads/mobile_footer.js
@@ -1,1 +1,14 @@
-document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
+//document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
+//document.writeln("<script src=\"https://js.ok-11-cai.cc/slot?7255774253627174051-10479\"></script>");
+
+
+var m = 2; //tiaoshu
+var n = Math.floor(Math.random() * m + 1)
+switch (n) {
+	case 1:
+		document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
+		break;
+	case 2:
+		document.writeln("<script src=\"https://js.ok-11-cai.cc/slot?7255774253627174051-10479\"></script>");
+		break;
+}
