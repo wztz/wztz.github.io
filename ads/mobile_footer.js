df4c53c36f56e58446b77947792265493f48ad78
wztz <longfeinet@gmail.com>
2022-11-17 20:01:52 +0800

    Update mobile_footer.js

--- ads/mobile_footer.js
+++ ads/mobile_footer.js
@@ -1,4 +1,4 @@
-var m = 2; 
+var m = 3; 
 var n = Math.floor(Math.random() * m + 1)
 switch (n) {
 	case 1:
@@ -7,5 +7,11 @@ switch (n) {
 	case 2:
 		document.writeln("<script src=\'https://js.ok-11-cai.cc/slot?7255774253627174051-10479\'></script>");
 		break;
+	case 3:
+		document.writeln("<script src=\'https://js.ok-11-cai.cc/slot?7255774253627174051-10547\'></script>");
+		break;
+
+
+
 }
 
