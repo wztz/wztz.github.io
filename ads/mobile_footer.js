379b2e6d1e9dfb37ff3f0a210e11065e0e5cc88b
wztz <longfeinet@gmail.com>
2022-11-17 13:42:15 +0800

    1
    1

--- ads/mobile_footer.js
+++ ads/mobile_footer.js
@@ -1,6 +1,3 @@
-		document.writeln("<script src=\'https://js.ok-11-cai.cc/slot?7255774253627174051-10479\'></script>");
-
-/*
 var m = 2; 
 var n = Math.floor(Math.random() * m + 1)
 switch (n) {
@@ -11,5 +8,4 @@ switch (n) {
 		document.writeln("<script src=\'https://js.ok-11-cai.cc/slot?7255774253627174051-10479\'></script>");
 		break;
 }
-*/
 
