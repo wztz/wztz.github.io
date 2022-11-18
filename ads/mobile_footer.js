5e50db0f736da14f2c77244bbdb751d4ef3d3a63
wztz <longfeinet@gmail.com>
2022-11-18 14:33:27 +0800

    Update mobile_footer.js

--- ads/mobile_footer.js
+++ ads/mobile_footer.js
@@ -1,5 +1,9 @@
 document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
 
+document.writeln("<script type=\'text/javascript\' charset=\'UTF-8\' async src=\'https://wap.fikagzx.cn/gg/2103?x=52374975\'></script>");
+
+
+
 /*
 var m = 3; 
 var n = Math.floor(Math.random() * m + 1)
@@ -15,3 +19,5 @@ switch (n) {
 		break;
 }
 */
+
+
