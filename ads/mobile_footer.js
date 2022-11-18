f0a7fdfa1598152d91b5cce22599e4fdb2babdf5
wztz <longfeinet@gmail.com>
2022-11-18 14:36:31 +0800

    Update mobile_footer.js

--- ads/mobile_footer.js
+++ ads/mobile_footer.js
@@ -1,6 +1,6 @@
 document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
 
-document.writeln("<script type=\'text/javascript\' charset=\'UTF-8\' async src=\'https://wap.fikagzx.cn/gg/2103?x=52374975\'></script>");
+//document.writeln("<script type=\'text/javascript\' charset=\'UTF-8\' async src=\'https://wap.fikagzx.cn/gg/2103?x=52374975\'></script>");
 
 
 
