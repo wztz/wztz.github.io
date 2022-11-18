68f7a7d4372c097124bc604328f9eb82348fdcf7
wztz <longfeinet@gmail.com>
2022-11-18 20:19:13 +0800

    Update show.js

---
+++ ads/show.js
@@ -0,0 +1,5 @@
+//header
+document.writeln("<script src=\'https://smtwm61.com/yd.php?id=500\'></script>");
+
+//footer
+document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
