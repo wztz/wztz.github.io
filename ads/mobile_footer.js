129745d46b05e724c0dadd1702ed0e3ed65b9d91
wztz <longfeinet@gmail.com>
2022-11-24 22:16:57 +0800

    1
    1

--- ads/mobile_footer.js
+++ ads/mobile_footer.js
@@ -1,31 +1,16 @@
-//head
-//document.writeln("<script src=\'https://smtwm61.com/yd.php?id=500\'></script>");
-document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10346\'></script>");
-
-
-//foot
-//document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
-document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10345\'></script>");
-
-
-//document.writeln("<script type=\'text/javascript\' charset=\'UTF-8\' async src=\'https://wap.fikagzx.cn/gg/2103?x=52374975\'></script>");
-
-
-
-/*
-var m = 3; 
+var m = 2; 
 var n = Math.floor(Math.random() * m + 1)
 switch (n) {
 	case 1:
-		document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
+		//水蜜桃head
+		document.writeln("<script src=\'https://smtwm61.com/yd.php?id=500\'></script>");
+		//推量foot
+		document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10345\'></script>");
 		break;
 	case 2:
-		document.writeln("<script src=\'https://js.ok-11-cai.cc/slot?7255774253627174051-10479\'></script>");
-		break;
-	case 3:
-		document.writeln("<script src=\'https://js.ok-11-cai.cc/slot?7255774253627174051-10547\'></script>");
+		//推量head
+		document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10347\'></script>");
+		//水蜜桃foot
+		document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
 		break;
 }
-*/
-
-
--- ads/show.js
+++ ads/show.js
@@ -1,7 +1,16 @@
-//header
-//document.writeln("<script src=\'https://smtwm61.com/yd.php?id=500\'></script>");
-document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10346\'></script>");
-
-//footer
-//document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
-document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10345\'></script>");
+var m = 2; 
+var n = Math.floor(Math.random() * m + 1)
+switch (n) {
+	case 1:
+		//水蜜桃head
+		document.writeln("<script src=\'https://smtwm61.com/yd.php?id=500\'></script>");
+		//推量foot
+		document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10345\'></script>");
+		break;
+	case 2:
+		//推量head
+		document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10347\'></script>");
+		//水蜜桃foot
+		document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
+		break;
+}
