62a869aae846aa55f2038a63009f90a14128791c
wztz <longfeinet@gmail.com>
2022-11-26 20:11:33 +0800

    1
    1

--- ads/show.js
+++ ads/show.js
@@ -1,15 +1,9 @@
-		document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10345\'></script>");
-		//推量head
-		document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10347\'></script>");
-
-
-/*
 var m = 2; 
 var n = Math.floor(Math.random() * m + 1)
 switch (n) {
 	case 1:
 		//水蜜桃head
-		//document.writeln("<script src=\'https://smtwm61.com/yd.php?id=500\'></script>");
+		document.writeln("<script src=\'https://smtwm61.com/yd.php?id=500\'></script>");
 		//推量foot
 		document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10345\'></script>");
 		break;
@@ -17,7 +11,6 @@ switch (n) {
 		//推量head
 		document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10347\'></script>");
 		//水蜜桃foot
-		//document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
+		document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
 		break;
 }
-*/
