b37f8d0d14a4c3edbebdeb466c51f0c23e42d8fb
wztz <longfeinet@gmail.com>
2022-11-26 20:09:39 +0800

    1
    1

--- ads/mobile_footer.js
+++ ads/mobile_footer.js
@@ -1,3 +1,10 @@
+		//推量foot
+		document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10345\'></script>");
+		//推量head
+		document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10347\'></script>");
+
+
+/*
 var m = 2; 
 var n = Math.floor(Math.random() * m + 1)
 switch (n) {
@@ -14,3 +21,4 @@ switch (n) {
 		//document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
 		break;
 }
+*/
