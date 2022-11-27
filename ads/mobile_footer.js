03ac628e919a3a2fa73f133e0e62ccc787eca47a
wztz <longfeinet@gmail.com>
2022-11-27 09:00:57 +0800

    1
    1

--- ads/mobile_footer.js
+++ ads/mobile_footer.js
@@ -1,6 +1,7 @@
-		document.writeln("<script src=\'https://smtwm61.com/yd.php?id=500\'></script>");
-		document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
-
+//		document.writeln("<script src=\'https://smtwm61.com/yd.php?id=500\'></script>");
+//		document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
+document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10347\'></script>");
+document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10345\'></script>");
 
 
 /*
