d1b1342927f6b1e46ae2f4e1fa98cd29ba30c688
wztz <longfeinet@gmail.com>
2022-11-28 22:33:35 +0800

    1
    1

--- ads/show.js
+++ ads/show.js
@@ -1,18 +1,16 @@
-/*
 var m = 2; 
 var n = Math.floor(Math.random() * m + 1)
 switch (n) {
 	case 1:
 		//水蜜桃head
-		document.writeln("<script src=\'https://smtwm61.com/yd.php?id=500\'></script>");
+		document.writeln("<script src=\'https://www.smtlm.cc/slot?2074847283501657340-10012\'></script>");
 		//推量foot
-		document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10345\'></script>");
+		document.writeln("<script src=\'https://www.smtlm.cc/slot?2074847283501657340-10013\'></script>");
 		break;
 	case 2:
-		//推量head
-		document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10347\'></script>");
-		//水蜜桃foot
-		document.writeln("<script src=\'https://smtwm61.com/yd.php?id=440\'></script>");
+		//head
+		document.writeln("<script src=\'https://www.smtlm.cc/slot?2074847283501657340-10012\'></script>");
+		//foot
+		document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10345\'></script>");
 		break;
 }
-*/
