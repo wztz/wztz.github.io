d788f3afe6267d1bd5355ef9336c4eb7ab133a25
wztz <longfeinet@gmail.com>
2022-12-01 21:44:03 +0800

    Update mobile_footer.js

--- ads/mobile_footer.js
+++ ads/mobile_footer.js
@@ -24,18 +24,18 @@ switch (n) {
 		//推量head
 		//	document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10347\'></script>");
 		//推量foot
-			document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10345\'></script>");
-		//水蜜桃foot
-		//	document.writeln("<script src=\'https://www.smtlm.cc/slot?2074847283501657340-10013\'></script>");
-		break;
-	case 2:
-		//水蜜桃head
-		//	document.writeln("<script src=\'https://www.smtlm.cc/slot?2074847283501657340-10012\'></script>");
-		//推量head
-			document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10347\'></script>");
-		//推量foot
 		//	document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10345\'></script>");
 		//水蜜桃foot
 			document.writeln("<script src=\'https://www.smtlm.cc/slot?2074847283501657340-10013\'></script>");
+		break;
+	case 2:
+		//水蜜桃head
+			document.writeln("<script src=\'https://www.smtlm.cc/slot?2074847283501657340-10012\'></script>");
+		//推量head
+		//	document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10347\'></script>");
+		//推量foot
+			document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10345\'></script>");
+		//水蜜桃foot
+		//	document.writeln("<script src=\'https://www.smtlm.cc/slot?2074847283501657340-10013\'></script>");
 		break;
 }
