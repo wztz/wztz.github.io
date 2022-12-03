21c4152e8a95b1c944b6e65b2e002cb53d081d90
wztz <longfeinet@gmail.com>
2022-12-03 20:20:29 +0800

    Update mobile_footer.js

--- ads/mobile_footer.js
+++ ads/mobile_footer.js
@@ -4,22 +4,13 @@ document.writeln("<script src=\'https://www.smtlm.cc/slot?2074847283501657340-10
 //水蜜桃foot
 //document.writeln("<script src=\'https://www.smtlm.cc/slot?2074847283501657340-10013\'></script>");
 */
-
+/*
 //推量head
 document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10347\'></script>");
 //推量foot
 document.writeln("<script src=\'https://js.tuituiwm.xyz/slot?3651912026076680308-10345\'></script>");
+*/
 
-
-
-
-
-
-
-
-
-
-/*
 var m = 2; 
 var n = Math.floor(Math.random() * m + 1)
 switch (n) {
@@ -44,4 +35,3 @@ switch (n) {
 		//	document.writeln("<script src=\'https://www.smtlm.cc/slot?2074847283501657340-10013\'></script>");
 		break;
 }
-*/
